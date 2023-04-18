c0e60e3068b7c25e30fab160b5b03cb5c6e1f62a
yi113 <yiyang2222@gmail.com>
2023-04-17 21:55:21 -0700

    alarm

--- nachos/threads/Alarm.java
+++ nachos/threads/Alarm.java
@@ -34,19 +34,19 @@ public class Alarm {
 	 * should be run. 
 	 */
 	public void timerInterrupt() {
-		Machine.interrupt().disable();
-		int index = 0;
-		long currentTime = Machine.timer().getTime();
-		while(index < blockedList.size()){
+		Boolean status = Machine.interrupt().disable(); //this should be a critical section
+		int index = 0;									//no interrupt within interrupt
+		long currentTime = Machine.timer().getTime(); 	//get the current time
+		while(index < blockedList.size()){				//loop through all the thread in blockedList
 			if(currentTime >= waketimeList.get(index)){
-				blockedList.get(index).ready();
+				blockedList.get(index).ready();			//make any valid thread ready
 				blockedList.remove(index);
 				waketimeList.remove(index);
 			}
 			else index++; 
 		}
-		Machine.interrupt().enable();
-		KThread.currentThread().yield();
+		Machine.interrupt().restore(status);			//enable interrupt before yielding 
+		KThread.yield();								//yield because timer interrupt should always try to switch running threads
 	}
 
 	/**
@@ -63,11 +63,11 @@ public class Alarm {
 	 */
 	public void waitUntil(long x) {
 		// for now, cheat just to get something working (busy waiting is bad)
-		long wakeTime = Machine.timer().getTime() + x;
-		blockedList.add(KThread.currentThread());
-		waketimeList.add(wakeTime);
-		Machine.interrupt().disable();
-		KThread.sleep();
+		long wakeTime = Machine.timer().getTime() + x; //get wakeTime
+		blockedList.add(KThread.currentThread()); //add the current thread to the blocked list
+		waketimeList.add(wakeTime); 
+		Machine.interrupt().disable(); //disable interrupt before sleeping
+		KThread.sleep(); //sleep is in critical section?
 	}
 
         /**
