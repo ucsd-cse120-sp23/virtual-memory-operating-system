34f9f8c7d74e44ee409569c874d2244f6e5836ae
AstraalWin <alow@ucsd.edu>
2023-04-23 14:04:35 -0700

    task3 in progress

--- nachos/threads/Condition2.java
+++ nachos/threads/Condition2.java
@@ -1,7 +1,8 @@
 package nachos.threads;
 
 import nachos.machine.*;
-
+import java.util.ArrayList;
+import java.util.LinkedList;
 /**
  * An implementation of condition variables that disables interrupt()s for
  * synchronization.
@@ -21,6 +22,9 @@ public class Condition2 {
 	 */
 	public Condition2(Lock conditionLock) {
 		this.conditionLock = conditionLock;
+
+		cvqueue = new ArrayList<KThread>();
+		//cvqueue = new LinkedList<KThread>();
 	}
 
 	/**
@@ -31,9 +35,12 @@ public class Condition2 {
 	 */
 	public void sleep() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		cvqueue.add(KThread.currentThread());
 
 		conditionLock.release();
-
+		boolean intstatus = Machine.interrupt().disable();
+		KThread.sleep();
+		Machine.interrupt().restore(intstatus);
 		conditionLock.acquire();
 	}
 
@@ -43,6 +50,13 @@ public class Condition2 {
 	 */
 	public void wake() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+
+		if (cvqueue.size() != 0) {
+			boolean intstatus = Machine.interrupt().disable();
+			KThread thrd = cvqueue.remove(0);
+			thrd.ready();
+			Machine.interrupt().restore(intstatus);
+		}
 	}
 
 	/**
@@ -51,6 +65,10 @@ public class Condition2 {
 	 */
 	public void wakeAll() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+
+		while (cvqueue.size() != 0) {
+			wake();
+		}
 	}
 
         /**
@@ -66,4 +84,62 @@ public class Condition2 {
 	}
 
         private Lock conditionLock;
+		// Place Condition2 testing code in the Condition2 class.
+
+    // Example of the "interlock" pattern where two threads strictly
+    // alternate their execution with each other using a condition
+    // variable.  (Also see the slide showing this pattern at the end
+    // of Lecture 6.)
+
+    private static class InterlockTest {
+        private static Lock lock;
+        private static Condition2 cv;
+
+        private static class Interlocker implements Runnable {
+            public void run () {
+                lock.acquire();
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(KThread.currentThread().getName());
+                    cv.wake();   // signal
+                    cv.sleep();  // wait
+                }
+                lock.release();
+            }
+        }
+
+        public InterlockTest () {
+            lock = new Lock();
+            cv = new Condition2(lock);
+			System.out.println("asdasd");
+
+            KThread ping = new KThread(new Interlocker());
+            ping.setName("ping");
+            KThread pong = new KThread(new Interlocker());
+            pong.setName("pong");
+
+            ping.fork();
+            pong.fork();
+
+            // We need to wait for ping to finish, and the proper way
+            // to do so is to join on ping.  (Note that, when ping is
+            // done, pong is sleeping on the condition variable; if we
+            // were also to join on pong, we would block forever.)
+            // For this to work, join must be implemented.  If you
+            // have not implemented join yet, then comment out the
+            // call to join and instead uncomment the loop with
+            // yields; the loop has the same effect, but is a kludgy
+            // way to do it.
+            ping.join();
+            // for (int i = 0; i < 50; i++) { KThread.currentThread().yield(); }
+        }
+    }
+
+    // Invoke Condition2.selfTest() from ThreadedKernel.selfTest()
+
+    public static void selfTest() {
+        new InterlockTest();
+    }
+
+	private ArrayList<KThread> cvqueue;
 }
+
