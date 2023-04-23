c5df5c840b7240ae006ff09dddc93a5033ef5d32
AstraalWin <alow@ucsd.edu>
2023-04-23 15:33:26 -0700

    task 3 done??

--- nachos/threads/Condition2.java
+++ nachos/threads/Condition2.java
@@ -37,11 +37,11 @@ public class Condition2 {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 		cvqueue.add(KThread.currentThread());
 
-		conditionLock.release();
 		boolean intstatus = Machine.interrupt().disable();
+		conditionLock.release();
 		KThread.sleep();
-		Machine.interrupt().restore(intstatus);
 		conditionLock.acquire();
+		Machine.interrupt().restore(intstatus);
 	}
 
 	/**
@@ -51,8 +51,8 @@ public class Condition2 {
 	public void wake() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 
+		boolean intstatus = Machine.interrupt().disable();
 		if (cvqueue.size() != 0) {
-			boolean intstatus = Machine.interrupt().disable();
 			KThread thrd = cvqueue.remove(0);
 			thrd.ready();
 			Machine.interrupt().restore(intstatus);
@@ -110,7 +110,6 @@ public class Condition2 {
         public InterlockTest () {
             lock = new Lock();
             cv = new Condition2(lock);
-			System.out.println("asdasd");
 
             KThread ping = new KThread(new Interlocker());
             ping.setName("ping");
@@ -138,6 +137,70 @@ public class Condition2 {
 
     public static void selfTest() {
         new InterlockTest();
+		cvTest5();
+    }
+	// Place Condition2 test code inside of the Condition2 class.
+
+    // Test programs should have exactly the same behavior with the
+    // Condition and Condition2 classes.  You can first try a test with
+    // Condition, which is already provided for you, and then try it
+    // with Condition2, which you are implementing, and compare their
+    // behavior.
+
+    // Do not use this test program as your first Condition2 test.
+    // First test it with more basic test programs to verify specific
+    // functionality.
+
+    public static void cvTest5() {
+        final Lock lock = new Lock();
+        // final Condition empty = new Condition(lock);
+        final Condition2 empty = new Condition2(lock);
+        final LinkedList<Integer> list = new LinkedList<>();
+
+        KThread consumer = new KThread( new Runnable () {
+                public void run() {
+                    lock.acquire();
+                    while(list.isEmpty()){
+                        empty.sleep();
+                    }
+                    Lib.assertTrue(list.size() == 5, "List should have 5 values.");
+                    while(!list.isEmpty()) {
+                        // context swith for the fun of it
+                        KThread.currentThread().yield();
+                        System.out.println("Removed " + list.removeFirst());
+                    }
+                    lock.release();
+                }
+            });
+
+        KThread producer = new KThread( new Runnable () {
+                public void run() {
+                    lock.acquire();
+                    for (int i = 0; i < 5; i++) {
+                        list.add(i);
+                        System.out.println("Added " + i);
+                        // context swith for the fun of it
+                        KThread.currentThread().yield();
+                    }
+                    empty.wake();
+                    lock.release();
+                }
+            });
+
+        consumer.setName("Consumer");
+        producer.setName("Producer");
+        consumer.fork();
+        producer.fork();
+
+        // We need to wait for the consumer and producer to finish,
+        // and the proper way to do so is to join on them.  For this
+        // to work, join must be implemented.  If you have not
+        // implemented join yet, then comment out the calls to join
+        // and instead uncomment the loop with yield; the loop has the
+        // same effect, but is a kludgy way to do it.
+        consumer.join();
+        producer.join();
+        //for (int i = 0; i < 50; i++) { KThread.currentThread().yield(); }
     }
 
 	private ArrayList<KThread> cvqueue;
